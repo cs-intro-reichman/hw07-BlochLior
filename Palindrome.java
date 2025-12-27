5e4a5d5b813d72cd61be54e37f2db7ce2feff37f
BlochLior <blochlior@gmail.com>
2025-12-27 16:57:01 +0200

    Palindrome done?

--- Palindrome.java
+++ Palindrome.java
@@ -2,12 +2,17 @@
 public class Palindrome {
 
 	public static void main(String[]args) {
-    	System.out.println(isPalindrome(args[0]));
+		System.out.println(isPalindrome(args[0]));
     }
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+		if (s.length() == 1 || s.length() == 0) {
+			return true;
+		}
+		// Helpful for my understanding on how i got it wrong at the start:
+		// System.out.println(s.substring(0,1) + " " + s.substring(s.length() - 1));
+		boolean result = s.substring(0, 1).equals(s.substring(s.length() - 1));
+		return result && isPalindrome(s.substring(1, s.length() - 1));
     }
 }
