8ecc3e159b3758b833d8acb5429bde60014ec7f6
BlochLior <blochlior@gmail.com>
2025-12-27 20:16:31 +0200

    Koch is Van Gogh

--- Koch.java
+++ Koch.java
@@ -2,41 +2,68 @@
 public class Koch {
 
 	public static void main(String[] args) {
-
-		//// Uncomment the first code block to test the curve function.
-		//// Uncomment the second code block to test the snowflake function.
-		//// Uncomment only one block in each test, and remember to compile
-		//// the class whenever you change the test.
-
-        /*
+        
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
-		curve(Integer.parseInt(args[0]),
-			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
-		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+		//curve(Integer.parseInt(args[0]),
+		//	  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
+		//      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+		
 
-		/*
+		
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		// I decided only to draw on the base case,
+		// then the erasure isn't required and it works more smoothly
+		// since the drawing only happens when reaching base,
+		// and as such no additional lines are being drawn that need to be
+		// erased. 
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+			return;
+		}
+		double x3 = (2 * x1 + x2) / 3.0;
+		double y3 = (2 * y1 + y2) / 3.0;
+		double x5 = (x1 + 2 * x2) / 3.0;
+		double y5 = (y1 + 2 * y2) / 3.0;
+		double x4 = 0.5 * (x3 + x5) - (Math.sqrt(3) / 2.0) * (y5 - y3);
+		double y4 = 0.5 * (y3 + y5) + (Math.sqrt(3) / 2.0) * (x5 - x3);
+		
+		// StdDraw.setPenColor(StdDraw.WHITE);
+		// StdDraw.line(x3, y3, x5, y5);
+		// StdDraw.setPenColor(StdDraw.BLACK);
+		// StdDraw.line(x3, y3, x4, y4);
+		// StdDraw.line(x4, y4, x5, y5);
+		curve(n - 1, x1, y1, x3, y3);
+		curve(n - 1, x3, y3, x4, y4);
+		curve(n - 1, x4, y4, x5, y5);
+		curve(n - 1, x5, y5, x2, y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
 		// A little tweak that makes the drawing look better
-		StdDraw.setYscale(0, 1.1);
-		StdDraw.setXscale(0, 1.1);
+		StdDraw.setYscale(-0.1, 1.1);
+		StdDraw.setXscale(-0.1, 1.1);
 		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		double x1 = 0.5;
+		double y1 = 1.0;
+		double x2 = 0.933;
+		double y2 = 0.25;
+		double x3 = 0.067;
+		double y3 = 0.25;
+
+		// Draw
+		curve(n, x1, y1, x2, y2);
+		curve(n, x2, y2, x3, y3);
+		curve(n, x3, y3, x1, y1);
 	}
 }
