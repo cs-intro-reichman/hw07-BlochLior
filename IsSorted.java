7205825bb58ef52909a3579cba22975b4d65dc02
BlochLior <blochlior@gmail.com>
2025-12-27 20:27:08 +0200

    IsItSorted?

--- IsSorted.java
+++ IsSorted.java
@@ -7,8 +7,12 @@ public class IsSorted {
 
     // Helper recursive function
     private static boolean isSorted(int[] a, int i) {
-        //// Replace the following statement with your code
-        return false; 
+        // Base case 1: empty or single element array
+        if (a.length <= 1) return true;
+        // Base case 2: reached the end and all is good
+        if (i == a.length - 1) return true;
+        boolean currentSorted = a[i] <= a[i + 1];
+        return currentSorted && isSorted(a, i+1); 
     }
 
     public static void main(String[] args) {
